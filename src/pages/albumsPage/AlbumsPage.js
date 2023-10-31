2dc9800f26858759f5e40bef443247f74ed1b43f
ptdat011002 <phamtridat01@gmail.com>
2023-10-31 13:44:25 +0700

    // màn album

--- src/pages/albumsPage/AlbumsPage.js
+++ src/pages/albumsPage/AlbumsPage.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
+import axios from "axios";
+import Album from "../../components/album/Album";
+import './AlbumsPage.css'
+
+const baseURL = "http://localhost:8080";
 
 function AlbumsPage() {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${baseURL}/albums`)
+      .then((res) => {
+        console.log(res.data);
+        setAlbums(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <Navbar />
       <div className="albums-page">
-        <h1>AlbumsPage</h1>
+        <div className="albums-list">
+          <div className="albums-new-title">Albums</div>
+          <div className="row">
+            {albums.map((x) => (
+              <div className="col-md-2" key={x.id}>
+                <Album albums={x} />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
